ca1d6e72fef791f44a4878edab4c1c4d4bef9ca9
viniciuslima04 <vinicius.moura@timebrisa.com.br>
2021-11-24 14:08:01 -0300

    Criação do front-and

---
+++ src/components/SignInButton/index.tsx
@@ -0,0 +1,25 @@
+import styles from './styles.module.scss';
+
+import { FaGithub } from 'react-icons/fa'
+import { FiX } from 'react-icons/fi'
+
+
+export function SignInButton() {
+
+  const isUserLoggedIn = true;
+
+  return isUserLoggedIn ? (
+    <button className={styles.signInButton} type="button">
+      <FaGithub color="#04d361" />
+      Vinícius Lima
+      <FiX color="737380" className={styles.closeIcon} />
+    </button>
+  ) : (
+
+    <button className={styles.signInButton} type="button">
+      <FaGithub color="#eba417" />
+      Sign in with GitHub
+      <FiX />
+    </button>
+  );
+}
